1fab671a7350e8003033cea836d11c8f832ab28d
IshidaKeisuke-algorithm <k.ishida@algorithms.co.jp>
2023-05-13 15:10:20 +0900

    リファクタしてデプロイ

--- pages/interview/index.tsx
+++ pages/interview/index.tsx
@@ -1,5 +1,5 @@
 import { InterviewCard } from "@/components/features/interview/InterviewItem";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { getAllInterviews } from "@/utils/hasura/interview";
 import { Interview } from "@/types/index";
 
@@ -7,21 +7,23 @@ interface Props {
   interviews: Interview[];
 }
 
-const Interview: NextPage<Props> = ({ interviews }) => {
-  return(
+const InterviewPage: NextPage<Props> = ({ interviews }) => {
+  return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '3rem' }}>
-      {interviews.map(interview => (
+      {interviews.map((interview) => (
         <InterviewCard key={interview.id} interview={interview} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps<Props> = async () => {
   const interviews = await getAllInterviews();
   return {
-    props: { interviews },
+    props: {
+      interviews,
+    },
   };
-}
+};
 
-export default Interview;
+export default InterviewPage;
